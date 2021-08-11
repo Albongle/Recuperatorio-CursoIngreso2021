3c6b98ad0a50950fc607f9ddcb5e7586c0e2060b
Albongle <alejandro.bongioanni@gmail.com>
2021-08-11 18:48:54 -0300

    Actualizacion

--- dos.js
+++ dos.js
@@ -1,136 +1,20 @@
 /*
-EJERCICIO 2
-El dueño de una tienda dedicada a la compra/venta de cartas de Yu-Gi-Oh! desea ingresar en el
-sistema las ventas realizadas en el dia de la fecha y conocer ciertos datos en base a las cartas
-que se vendieron.
-Se ingresara hasta que el usuario decida:
-* Nombre de la carta.
-* Tipo de carta: Validar "monstruo", "magica", "trampa".
-* Rareza: Validar "rara", "super rara", "ultra rara".
-* Precio: Validar que no sea 0 o negativo.
+Ejercicio 2
+El dueño de una tienda de revistas desea un sistema para el control de stock de su negocio.
+Para ello se debe ingresar hasta que el usuario desee:
+1.	Tipo de revista (Ocio, Científica, Informativas), validar.
+2.	Genero (Deporte, Tecnología, Moda, Hogar), validar.
+3.	Nombre de la revista.
+4.	Año de lanzamiento (entre 2010 y 2021).
 Se pide informar por document.write:
-A) El nombre y rareza  de la carta tipo "trampa" con mayor precio.
+A.	El nombre y tipo de revista con género “Tecnología” mas antiguo en el sistema.
+B.	Cuantas revistas de género “Moda” y que son de “Ocio” poseen fecha de lanzamiento previo al 2015.
+C.	El promedio de año de lanzamiento para las revistas de tipo Cientifica.
 
-B) Cuantas cartas de rareza "ultra rara" y de tipo "magica" o "monstruo" fueron vendidas.
 
-C) El promedio de precio de las cartas de por tipo.
 */
 
 function mostrar()
 {
-	var nombreDeLaCarta;
-	var tipoDeCarta;
-	var rareza;
-	var precio;
-	var precioCartaTrampaMasCara;
-	var nombreCartaTrampaMasCara;
-	var rarezaCartaTrampaMasCara;
-	var banderaPrimerIngreso;
-	var acumuladorPrecioMagicas;
-	var acumuladorPrecioMonstruo;
-	var acumuladorPrecioTrampa;
-	var promedioPrecioMagicas;
-	var promedioPrecioMonstruo;
-	var promedioPrecioTrampa;
-	var contadorMagicas;
-	var contadorTrampa;
-	var contadorMonstruo;
-	var contadorCartasUltraRara;
-	var mensaje;
 
-	respuesta=true;
-	banderaPrimerIngreso=true;
-	acumuladorPrecioMagicas=0;
-	acumuladorPrecioMonstruo=0;
-	acumuladorPrecioTrampa=0;
-	promedioPrecioTrampa=0;
-	promedioPrecioMonstruo=0;
-	promedioPrecioMagicas=0;
-	contadorMonstruo=0;
-	contadorTrampa=0;
-	contadorMagicas=0;
-	contadorCartasUltraRara=0;
-
-
-	while(respuesta==true)
-	{
-		nombreDeLaCarta=prompt("Ingrese nombre de la carta");
-
-		tipoDeCarta=prompt("Ingrese tipo de carta","monstruo, magica o trampa");
-		while(tipoDeCarta!="monstruo"&&tipoDeCarta!="magica"&&tipoDeCarta!="trampa")
-		{
-			tipoDeCarta=prompt("Ingrese tipo de carta","monstruo, magica o trampa");
-		}
-
-		rareza=prompt("Ingrese nivel de rareza","rara, super rara o ultra rara");
-		while(rareza!="rara"&&rareza!="super rara"&&rareza!="ultra rara")
-		{
-			rareza=prompt("Ingrese nivel de rareza","rara, super rara o ultra rara");
-		}
-
-		precio=prompt("Ingrese precio","valor mayor a cero");
-		precio=parseInt(precio);
-		while(isNan(precio)||precio<0)
-		{
-			precio=prompt("Ingrese precio","valor mayor a cero");
-			precio=parseInt(precio);
-		}
-
-		switch(tipoDeCarta)
-		{
-			case "magica":
-			acumuladorPrecioMagicas=acumuladorPrecioMagicas+precio;
-			contadorMagicas++;
-			if(rareza=="ultra rara")
-			{
-				contadorCartasUltraRara++;
-			}
-			
-			break;
-			case "monstruo":
-			acumuladorPrecioMonstruo=acumuladorPrecioMonstruo+precio;
-			contadorMonstruo++;
-			if(rareza=="ultra rara")
-			{
-				contadorCartasUltraRara++;
-			}
-			break;
-			case "trampa":
-			if(banderaPrimerIngreso==true||precioCartaTrampaMasCara<precio)
-			{
-				banderaPrimerIngreso=false;
-				nombreCartaTrampaMasCara=nombre;
-				rarezaCartaTrampaMasCara=rareza;
-				precioCartaTrampaMasCara=precio;
-				
-			}
-			acumuladorPrecioTrampa=acumuladorPrecioTrampa+precio;
-			contadorTrampa++;
-			break;
-		}
-		respuesta=confirm("¿Desea seguir?");
-	}
-
-	if(contadorTrampa>0)
-	{
-		promedioPrecioTrampa=acumuladorPrecioTrampa/contadorTrampa;
-	}
-	if(contadorMagicas>0)
-	{
-		promedioPrecioMagicas=acumuladorPrecioMagicas/contadorMagicas;
-	}
-	if(contadorMonstruo>0)
-	{
-		promedioPrecioMonstruo=acumuladorPrecioMonstruo/contadorMonstruo;
-	}
-
-	if(contadorTrampa>0)
-	{
-		mensaje="La carta tipo trampa con mayor precio se llama "+nombreCartaTrampaMasCara+" y es "+rarezaCartaTrampaMasCara;
-	}
-	
-	mensaje=mensaje+". La cantidad de cartas vendidas de rareza ultra rara y de tipo magica o monstruo es: "+contadorCartasUltraRara;
-	mensaje=mensaje+". El promedio de precio de las cartas trampa es: "+promedioPrecioTrampa+", el de las cartas magicas es: "+promedioPrecioMagicas+" y el de las tipo monstruo es :"+promedioPrecioMonstruo;
-
-	document.write(mensaje);
 }
--- tres.js
+++ tres.js
@@ -1,19 +1,18 @@
 /*
-debemos cargar 10 productos que acaban de llegar a la empresa 
-"Galletitas Felices", para cada producto se debe registrar:
-*marca del producto
-* Tipo de galletita : "con sal", "sin sal", "dulces".
-* cantidad de cajas: mas de 0
-* cantidad de kilos por caja:mas de 0
-* nombre proveedor
-* origen: "nacional", "mercosur", "resto del mundo"
+Ejercicio 3
+Debemos cargar 5 productos que acaban de llegar a la empresa “Tomando Ando”, para cada producto se debe registrar:
+1.	Tipo de bebida: (Con Azucar, Sin Azúcar, Light)
+2.	Cantidad de Packs: más de 0.
+3.	Cantidad de botellas por Pack: más de 0.
+4.	Nombre proveedor.
+5.	Origen: (Nacional, Mercosur, Resto del mundo).
 Se pide informar por document.write:
-a) el tipo con la mayor cantidad de cajas.
-b) de las galletitas de origen nacional, la que menos kilos tiene por caja.
-c) que porcentaje de cajas hay sobre el total de cada origen 
-[ej: 30% nacional ,30% mercosur, 40% resto del mundo ( debe sumar 100)]
-*/
+A.	El tipo con mayor cantidad packs, de origen nacional.
+B.	De las bebidas de origen “Resto del mundo”, la que menos botellas tiene por pack.
+C.	El promedio de botellas por origen.
 
+
+*/
 function mostrar()
 {
 	alert("tres");
